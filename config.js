52dee1d885bc4810412acde3dbdbc95071f197f3
Richard Miles <richlloydmiles@Richards-MacBook-Pro.local>
2016-05-16 22:16:54 +0200

    init

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	'HOSTPATH': 'http://dry-caverns-69060.herokuapp.com/',
+	'PORT': 9090,
+	'EXPRESS_SESSION_SECRET': 'thisisasecret',
+	'WP_CONSUMER_KEY': '7Dhqd0VIPevB',
+	'WP_CONSUMER_SECRET': 'YeIhqX8qmQD94bA7nn5TjshltJ7hK829Qj2rCX10ciYNmZlo',
+	'WP_INDEX_ENDPOINT': 'http://richmiles.co.za/wp-json/'
+};
+
+
+
+            // public: '7Dhqd0VIPevB',
+            // secret: 'YeIhqX8qmQD94bA7nn5TjshltJ7hK829Qj2rCX10ciYNmZlo'
